99532d17a3a4784d4546c5abed6173de39cbe7c2
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-08-25 20:56:13 -0500

    Clases empresa y empleado ok

--- src/main/java/com/udea/exchangehouse/ExchangeHouseApplication.java
+++ src/main/java/com/udea/exchangehouse/ExchangeHouseApplication.java
@@ -1,5 +1,6 @@
 package com.udea.exchangehouse;
 
+import com.udea.exchangehouse.models.Empleado;
 import com.udea.exchangehouse.models.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,12 @@ public class ExchangeHouseApplication {
         Empresa empresa1 = new Empresa("Exchange House Coin", "Bogota", "3600566", "9002858585");
         String informacion = "<h1>Informacion de la empresa</h1>" + "<br><b>Nombre de la empresa: </b>" + empresa1.getNombre() + "<br><b>Direccion: </b>" + empresa1.getDireccion() + "<br><b>Telefono: </b>" + empresa1.getTelefono() + "<br><b>NIT: </b>" + empresa1.getNit();
         return informacion;
+    }
+
+    @GetMapping("/empleado")
+    public String empleado(){
+        Empleado empleado1 = new Empleado();
+        return "Empleado";
     }
 
     public static void main(String[] args) {
