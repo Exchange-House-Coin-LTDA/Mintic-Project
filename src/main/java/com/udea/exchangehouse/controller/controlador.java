324a4eb517748e3c31d77eca00cd0726e00ccf7d
Cristhian Riveros <riveros.kristianr@gmail.com>
2022-09-03 14:39:29 -0500

    se agrega servicios y controlador de la clase empresa

---
+++ src/main/java/com/udea/exchangehouse/controller/controlador.java
@@ -0,0 +1,50 @@
+package com.udea.exchangehouse.controller;
+
+import com.udea.exchangehouse.models.Empresa;
+import com.udea.exchangehouse.services.EmpresaServ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class controlador {
+    @Autowired
+    EmpresaServ empresaServ;
+
+    @GetMapping("/enterprises")
+    public List<Empresa> verEmpresas(){
+        return empresaServ.todasLasEmpresas();
+    }
+
+    @PostMapping("/enterprises")
+    public Empresa guardarEmpresa(@RequestBody Empresa emp){
+        return this.empresaServ.guardarActualizarEmpresa(emp);
+    }
+
+    @GetMapping("/enterprises/{id}")
+    public Empresa empresaPorID(@PathVariable("id") Integer id){
+        return this.empresaServ.empresaPorId(id);
+    }
+
+    @PatchMapping("/enterprises/{id}")
+    public Empresa actualizarEmpresa(@PathVariable("id") Integer id, @RequestBody Empresa empresa){
+        Empresa emp= empresaServ.empresaPorId(id);
+        emp.setNombre(empresa.getNombre());
+        emp.setDireccion(empresa.getDireccion());
+        emp.setTelefono(empresa.getTelefono());
+        emp.setNit(empresa.getNit());
+        return empresaServ.guardarActualizarEmpresa(emp);
+    }
+
+    @DeleteMapping ("/enterprises/{id}")
+    public String deleteEmpresa(@PathVariable("id") Integer id){
+        boolean respuesta= empresaServ.borrarEmpresa(id);
+        if (respuesta){
+            return "Se elimino la empresa con id" +id;
+        }
+        else{
+            return "No se pudo eliminar la empresa con id"+id;
+        }
+    }
+}
