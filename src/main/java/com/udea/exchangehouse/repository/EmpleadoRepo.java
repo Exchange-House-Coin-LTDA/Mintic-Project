fd0ce01ac705b1c6bbbd31bd9447a3d0256f4b10
jsebastian2197 <jsebastian.t97@gmail.com>
2022-09-06 20:44:58 -0500

    Servicios y repo para la clase empleado

--- src/main/java/com/udea/exchangehouse/repository/EmpleadoRepo.java
+++ src/main/java/com/udea/exchangehouse/repository/EmpleadoRepo.java
@@ -1,9 +1,9 @@
 package com.udea.exchangehouse.repository;
 
-import com.udea.exchangehouse.models.Empresa;
+import com.udea.exchangehouse.models.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmpresaRepo extends JpaRepository <Empresa, Integer> {
+public interface EmpleadoRepo extends JpaRepository <Empleado, Integer> {
 }
