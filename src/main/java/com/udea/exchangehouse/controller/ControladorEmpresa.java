033d8f2056061ab4a1f2a70aa4da8a873b2f5c89
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-09-16 19:44:27 -0500

    Cambio a controler de entidad empresa

--- src/main/java/com/udea/exchangehouse/controller/ControladorEmpresa.java
+++ src/main/java/com/udea/exchangehouse/controller/ControladorEmpresa.java
@@ -3,13 +3,15 @@ package com.udea.exchangehouse.controller;
 import com.udea.exchangehouse.DTO.EmpresaDTO;
 import com.udea.exchangehouse.models.Empresa;
 import com.udea.exchangehouse.services.EmpresaServ;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class ControladorEmpresa {
     private final EmpresaServ empresaServ;
 
@@ -17,38 +19,54 @@ public class ControladorEmpresa {
         this.empresaServ = empresaServ;
     }
 
+    //Consultar empresas
     @GetMapping("/enterprises")
-    public List<Empresa> verEmpresas(){
-        return empresaServ.todasLasEmpresas();
+    public String verEmpresas(Model model){
+        List<Empresa> empresas = this.empresaServ.todasLasEmpresas();
+        List<EmpresaDTO> empresasDTO = new ArrayList<>();
+        empresas.forEach(empresa -> empresasDTO.add(new EmpresaDTO(empresa.getId(),
+                empresa.getNombre(),empresa.getDireccion(),empresa.getTelefono(),empresa.getNit(),
+                empresa.getEmpleados())));
+        model.addAttribute("empresas", empresasDTO);
+        return "verEmpresas";
     }
 
+    //Agregar empresa
+    @GetMapping("/adEnterprise")
+    public String nuevaEmpresa(Model model){
+        EmpresaDTO empresa = new EmpresaDTO();
+        model.addAttribute("empresa", empresa);
+        return "agregarEmpresa";
+    }
+
+    //Guardar empresa
     @PostMapping("/enterprises")
-    public Empresa guardarEmpresa(@RequestBody EmpresaDTO empresaDTO){
-        return this.empresaServ.saveEmpresa(empresaDTO);
+    public String guardarEmpresa(EmpresaDTO empresaDTO){
+        System.out.println(empresaDTO);
+        this.empresaServ.saveEmpresa(empresaDTO);
+        return "redirect:/enterprises";
     }
 
+    //Editar empresa por ID
     @GetMapping("/enterprises/{id}")
-    public Optional<Empresa> empresaPorID(@PathVariable("id") Integer id){
-        return this.empresaServ.empresaPorId(id);
+    public String empresaPorID(@PathVariable("id") Integer id, Model model){
+        Empresa empresa = this.empresaServ.empresaPorId(id).get();
+        EmpresaDTO empresaDTO = new EmpresaDTO(empresa.getId(), empresa.getNombre(), empresa.getDireccion(),
+                empresa.getTelefono(), empresa.getNit(), empresa.getEmpleados());
+        model.addAttribute("empresa", empresaDTO);
+        return "editarEmpresa";
     }
 
-    @PatchMapping("/enterprises/{id}")
-    public Empresa actualizarEmpresa(@PathVariable("id") Integer id, @RequestBody EmpresaDTO empresaDTO){
-        if(this.empresaServ.empresaPorId(id).isPresent()){
-            empresaDTO.setId(id);
-            return empresaServ.updateEmpresa(empresaDTO);
-        }
-        return null;
+    @PostMapping("/updateEnterprise")
+    public String actualizarEmpresa(EmpresaDTO empresaDTO){
+        this.empresaServ.updateEmpresa(empresaDTO);
+        return "redirect:/enterprises";
     }
 
-    @DeleteMapping ("/enterprises/{id}")
+    @GetMapping ("/deleteEnterprise/{id}")
     public String deleteEmpresa(@PathVariable("id") Integer id){
-        boolean respuesta= empresaServ.borrarEmpresa(id);
-        if (respuesta){
-            return "Se elimino la empresa con id" +id;
-        }
-        else{
-            return "No se pudo eliminar la empresa con id"+id;
-        }
+        this.empresaServ.borrarEmpresa(id);
+        return "redirect:/enterprises";
+
     }
 }
